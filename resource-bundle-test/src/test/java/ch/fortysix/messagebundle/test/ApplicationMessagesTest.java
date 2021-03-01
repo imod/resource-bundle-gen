3e1fb8d32a30041129e7ba77b67071c0d6564114
Dominik Bartholdi <domi@fortysix.ch>
2021-03-01 21:57:13 +0100

    add tests with german

--- resource-bundle-test/src/test/java/ch/fortysix/messagebundle/test/ApplicationMessagesTest.java
+++ resource-bundle-test/src/test/java/ch/fortysix/messagebundle/test/ApplicationMessagesTest.java
@@ -15,16 +15,19 @@ class ApplicationMessagesTest {
 	@Test
 	void testNoArgument() {
 		assertEquals("secondvalue", messages.anotherkey(Locale.ENGLISH));
+		assertEquals("zweiter wert", messages.anotherkey(Locale.GERMAN));
 	}
 
 	@Test
 	void testWithOneArgument() {
 		assertEquals("Hello World", messages.keyWithDots(Locale.ENGLISH, "World"));
+		assertEquals("Hallo Welt", messages.keyWithDots(Locale.GERMAN, "Welt"));
 	}
 
 	@Test
 	void testWithThreeArgument() {
 		assertEquals("one sometext two three", messages.keyWithUnderlines(Locale.ENGLISH, "one", "two", "three"));
+		assertEquals("eins ein text zwei drei", messages.keyWithUnderlines(Locale.GERMAN, "eins", "zwei", "drei"));
 	}
 
 }
--- resource-bundle-test/src/test/java/ch/fortysix/messagebundle/test/ServiceBundleTest.java
+++ resource-bundle-test/src/test/java/ch/fortysix/messagebundle/test/ServiceBundleTest.java
@@ -15,16 +15,19 @@ class ServiceBundleTest {
 	@Test
 	void testWithTwoArgument() {
 		assertEquals("By Hans Muster", bundle.sayBy(Locale.ENGLISH, "Hans", "Muster"));
+		assertEquals("Auf wiedersehen Hans Muster", bundle.sayBy(Locale.GERMAN, "Hans", "Muster"));
 	}
 
 	@Test
 	void testWithOneArgument() {
 		assertEquals("Hello Hans", bundle.sayHi(Locale.ENGLISH, "Hans"));
+		assertEquals("Hallo Erich", bundle.sayHi(Locale.GERMAN, "Erich"));
 	}
 
 	@Test
 	void testWithNoArgument() {
 		assertEquals("somevalue", bundle.somekey(Locale.ENGLISH));
+		assertEquals("einwert", bundle.somekey(Locale.GERMAN));
 	}
 
 }
